38b8f7c4f54e1d0fe92e56c3e2c6191d1d07020c
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-04-10 15:28:50 -0300

    feat: create List funcioanrios

---
+++ src/main/java/com/aula01/main/controller/FuncionarioController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.aula01.main.controller;
+
+import com.aula01.main.dao.FuncionarioDAO;
+import com.aula01.main.model.FuncionarioBean;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ *
+ * @author gaigu
+ */
+@Controller
+public class FuncionarioController {
+    FuncionarioDAO d = new FuncionarioDAO();
+    
+    @GetMapping("/funcionarios")
+    public String funcionarios(Model model) {
+        List<FuncionarioBean> lista =  d.listarFuncionarios();
+        model.addAttribute("lista", lista);
+        return "funcionarios";
+    }
+}
